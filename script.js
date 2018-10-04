1031f98d58373ebeb59f30e5ce6b2edafbc926bb
Nafiz Sadman <nafizsadman420@gmail.com>
2018-10-04 03:13:27 +0000

    1620487 JS EX 2

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 /* JavaScript code here */
 console.log("External JS");
+
+
+function sumArray(x)
+{
+    var sum=0;
+    x.forEach(function(value, index) {
+	sum = sum + value;
+    });
+	return sum;
+}
+console.log(sumArray([1,2,3]));
