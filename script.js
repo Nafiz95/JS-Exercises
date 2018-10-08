3c2227b7e02b4957f00f541fcd1e658d25bfa99e
Nafiz Sadman <nafizsadman420@gmail.com>
2018-10-08 02:30:44 +0000

    1620487 JS EX3

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 /* JavaScript code here */
+//Task 1
 console.log("External JS");
 
-
+//Task 2
 function sumArray(x)
 {
     var sum=0;
@@ -11,3 +12,14 @@ function sumArray(x)
 	return sum;
 }
 console.log(sumArray([1,2,3]));
+
+//Task 3
+function emailvalidation(address)
+{
+
+    var check=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match = check.test(address);
+    return match;
+}
+console.log(emailvalidation("akib@9pm.com"));
+
